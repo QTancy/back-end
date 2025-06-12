b67002c4e9d420d8e45a141dabc3418cdc3af1ec
wynprtm <wayanpratama38@gmail.com>
2025-06-12 14:46:29 +0700

    fix: :wrench: add cors configuration

--- server.js
+++ server.js
@@ -7,9 +7,29 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+    'https://qtancy.netlify.app/', // Ganti dengan URL Netlify FRONTEND Anda
+    'http://localhost:3000',                 // Untuk pengembangan frontend lokal
+    
+];
 
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true, 
+    optionsSuccessStatus: 200 
+}));
+
+app.use(express.json());
 app.use(router);
 
 app.listen(PORT, ()=> {
